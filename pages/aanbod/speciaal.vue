a61f39dafb6a56b923b7d27a3da9b0c9f4f74fed
MarCanIT <info@MarCanIT.be>
2022-01-27 00:26:23 +0100

    pagina speciaal gemaakt

---
+++ pages/aanbod/speciaal.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-row class="py-10">
+      <v-col>
+        <h1 class="text-h3">
+          <span class="spellebel-title">Puzzels (P)</span>
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn text to="/aanbod" exact>
+          <v-icon left>{{ icons.arrowLeft }}</v-icon>
+          Terug naar overzicht
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p class="text-body-1">
+          Dit zijn maar enkele voorbeelden van ons aanbod. Kom zeker eens langs
+          om alles te ontdekken!
+        </p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <!-- Start van een product -->
+      <v-col cols="12" sm="4" md="3">
+        <v-card>
+          <v-img src="/img/binnen.jpg" class="align-end" height="300px">
+          </v-img>
+        </v-card>
+      </v-col>
+      <!-- Einde van een product -->
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn text to="/aanbod" exact>
+          <v-icon left>{{ icons.arrowLeft }}</v-icon>
+          Terug naar overzicht
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+import { mdiArrowLeft } from '@mdi/js'
+
+export default Vue.extend({
+  data: () => ({
+    icons: {
+      arrowLeft: mdiArrowLeft,
+    },
+  }),
+
+  head(): MetaInfo {
+    return {
+      title: 'Puzzels',
+    }
+  },
+})
+</script>
