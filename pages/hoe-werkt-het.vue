ed476e3e5ab724c0d73716a4000da9c693db7cc1
MarCanIT <info@MarCanIT.be>
2022-02-03 20:22:22 +0100

    Text-changes HOE WERKT HET

--- pages/hoe-werkt-het.vue
+++ pages/hoe-werkt-het.vue
@@ -3,11 +3,11 @@
     <v-row class="py-10">
       <v-col>
         <h1 class="text-h3">
-          <span class="spellebel-title"> Hoe werkt het? </span>
+          <span class="spellebel-title"> Uitlenen : </span>
         </h1>
       </v-col>
     </v-row>
-    <v-row>
+   <v-row>
       <v-col>
         <h2 class="text-h4">Abonnement</h2>
       </v-col>
@@ -15,21 +15,72 @@
     <v-row>
       <v-col class="text-body-1">
         <p>
-          Naargelang je abonnement, kan je 3 of 6 stuks speelgoed uit ons
-          assortiment kiezen. Het speelgoed dat bij ons in de rekken staat heeft
-          altijd een bijgevoegd onderdelenblad. Hierop staat alles wat er in je
-          spel of doos zit. Wanneer je het speelgoed terug brengt kan je thuis
-          alles nog eens natellen, eventueel verloren onderdelen weer bij elkaar
-          zoeken en op die manier weer alles volledig bij ons binnen brengen.
+          De uitleenperiode bedraagt maximaal 3 weken, gerekend vanaf de dag van lenen. 
+          Mocht het einde van de uitleenperiode vallen in een sluitingsperiode van de speelotheek (bijvoorbeeld vanwege vakantie) 
+          dan wordt de uiterste terugbrengdatum automatisch aangepast.
         </p>
         <p>
-          Wanneer je het speelgoed bij ons inlevert zullen wij dit alsnog
-          natellen. Zo kunnen wij steeds garant staan voor volledig en
-          kwaliteitvol speelgoed dat opnieuw uitgeleend kan worden. Onze
-          uitleenperiode is altijd 3 weken, je mag alsnodig 1 keer verlengen.
-          Dat kan je via
-          <nuxt-link to="/contact">sms/telefoon/voicemail/mail</nuxt-link>
-          vermeld altijd zeker je naam en lidnummer.
+          Bij elke uitlening krijg je een overzicht van wat je meeneemt.
+          <br>Hierop staat ook de inleverdatum.
+        </p>
+        <p>
+          Bij ons speelgoed zit ook een overzicht met wat er in zit. 
+        </p>
+        <p>
+          Wanneer je het speelgoed bij ons inlevert zullen wij dit natellen. 
+          Zo kunnen wij steeds garant staan voor volledig speelgoed dat opnieuw uitgeleend kan worden. 
+        </p>
+        <p>
+          Bij te laat inleveren of missende onderdelen, wordt een bijdrage gevraagd.
+        </p>
+      </v-col>
+    </v-row>
+      <v-row>
+      <v-col>
+        <h2 class="text-h4">Abonnementen :</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-body-1">
+        <p>
+          Wij werken met 3 types abonnement :
+        </p>
+        <p>
+         <table border=0>
+           <tr>
+              <td>
+                Gewoon abonnement
+              </td>
+              <td>
+                3 stuks
+              </td>
+              <td>
+                €15
+              </td>
+           </tr>
+           <tr>
+              <td>
+                XL-abonnement
+              </td>
+              <td>
+                6 stuks
+              </td>
+              <td>
+                €25
+              </td>
+           </tr>
+           <tr>
+              <td>
+                Abonnement D-Kaart
+              </td>
+              <td>
+                3 stuks
+              </td>
+              <td>
+                €5
+              </td>
+           </tr>
+         </table>
         </p>
       </v-col>
     </v-row>
@@ -41,9 +92,15 @@
     <v-row>
       <v-col class="text-body-1">
         <p>
-          De uitleenperiode van een speelgoed kan na normale uitleentermijn zonodig éénmalig verlengd worden 
-          met één uitleenperiode onder de voorwaarde dat het betreffende spelmateriaal niet gereserveerd is 
+          De uitleenperiode kan 1 keer verlengd worden, onder de voorwaarde dat het betreffende spelmateriaal niet gereserveerd is 
           door een andere ontlener.
+        </p>
+        <p>
+          Verlengen kan tot sluitingstijd op je inleverdag. Dit kan telefonisch of via sms / Whatsapp.
+          <br>Vermeld altijd zeker je naam en lidnummer !
+        </p>
+        <p>
+          Verlengen via mail of facebook/messenger is momenteel nog niet mogelijk.
         </p>
       </v-col>
     </v-row>
