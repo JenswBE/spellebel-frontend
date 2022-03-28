8f244a5ccb9c9a21e0fbdf85fb8412e6ea9a43c8
MarCanIT <info@MarCanIT.be>
2022-03-28 23:26:41 +0200

    Bullets tasks RVB

--- pages/wie-zijn-we.vue
+++ pages/wie-zijn-we.vue
@@ -16,7 +16,7 @@
       <v-col class="text-body-1">
         <p>
           Wij noemen onze vrijwilligers SPULMERS.
-          <br>Dit letterwoord komt van "SPeel-o-theekUitLeenMedewekERS" 
+          <br>Dit letterwoord komt van "SPeel-o-theekUitLeenMedewerkERS" 
         </p>
         <p>
           Enkele vrijwilligers uit ons spulmerteam staan elke zaterdagvoormiddag en elke woensdagnamiddag klaar, om jullie te ontvangen bij onze Speel-o-theek Spellebel. 
@@ -26,18 +26,18 @@
           Onze spulmers nemen het in te leveren speelgoed weer in ontvangst, dit wordt netjes nageteld en nadien weer op de juiste plaats in onze rekken geplaatst.
           Het nieuwgekozen speelgoed wordt door hen ook weer in onze computer ingevoerd. 
         <p>
-          Tussendoor, en eigenlijk het meest belangrijke is, een tasje koffie, thee of iets fris te drinken en gewoon lekker bij te kletsen. 
-          Want voor dat sociaal contact en om er te zijn voor onze leden zijn onze spulmers al jaren vrijwilliger bij onze Speel-o-theek.
+          Tussendoor, en eigenlijk het meest belangrijke: een tasje koffie, thee of iets fris drinken en gewoon lekker bijkletsen. 
+          Want voor dat sociaal contact en om er te zijn voor onze leden, zijn onze spulmers al jaren vrijwilliger bij onze Speel-o-theek.
         </p>
         <p>
-          Wie al vaker bij ons is geweest zal zien dat er regelmatig dezelfde teams van spulmers samen ingepland zijn. 
+          Wie al vaker bij ons is geweest, zal zien dat er regelmatig dezelfde teams van spulmers samen ingepland zijn. 
           Dit komt doordat wij zelf ons team mogen samenstellen. 
           Vaak zijn dat al kennissen, vrienden, of zelfs familie die elkaar goed aanvullen tot een top-team, 
           met allemaal hun eigen talenten en uiteraard een warm hart voor onze doelgroep.
         </p>
         <p>
           Onze vrijwilligers komen 2 keer per jaar samen om de agenda in te plannen. Wij kiezen dus zelf wanneer we komen spulmeren. 
-          Daar bovenop is er jaarlijks een spulmeractiviteit waar al onze spulmers nog eens op een ludieke manier samenkomen, een hapje eten, drinken, 
+          Daarbovenop is er jaarlijks een spulmeractiviteit waar al onze spulmers nog eens op een ludieke manier samenkomen, een hapje eten, drinken, 
           lachen en genieten van onze fijne teamsfeer.
         </p>
         <p>
@@ -55,18 +55,23 @@
       <v-col class="text-body-1">
         <p>
           An, Gery, Hild, Ine, Mark, Monique, Ria, Sarah en Tine steken maandelijks de koppen bij elkaar om al wat achter de schermen moet gebeuren in goede banen te leiden.  
-          Zij vormen het bestuur van Spellebel. Zij waken over een breed en kwaliteitsvol aanbod van speelgoed en zij leggen contacten met de spulmers en met andere instanties.  
+          Zij vormen samen het bestuur van Spellebel. Zij waken over een breed en kwaliteitsvol aanbod van speelgoed en leggen contacten met de spulmers en met andere instanties.  
         </p>
         <p>
+          De RVB ...
+          <ul>
+            <li>... stuurt !</li>
+            <li>... filtert ideeën</li>
+            <li>... werkt achter de schermen</li>
+            <li>... regelt de administratie</li>
+            <li>... zorgt voor de centen</li>
+            <li>... lost problemen op waar die zich stellen</li>
+            <li>... organiseert 2 x per jaar een algemene vergadering</li>
+            <li>... zorgt ervoor dat Spellebel Spellebel kan zijn !</li>
         </p>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="12" lg="10" offset-lg="1">
-        <v-img src="/img/team-small.jpg"></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
+   </v-container>
 </template>
 
 <script lang="ts">
