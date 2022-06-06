baa8b7408ddd7ab982373b1b7227398b00af6f10
MarCanIT <info@MarCanIT.be>
2022-06-06 14:44:36 +0200

    Wie zijn we

--- pages/wie-zijn-we.vue
+++ pages/wie-zijn-we.vue
@@ -45,7 +45,7 @@
         </p>
         <p>
           Kom jij graag ons vrijwilligersteam versterken ?
-          <br>Neem dan alvast een kijkje op <nuxt-link to="/spulmer-worden">deze (nog te maken) pagina</nuxt-link> !
+          <br>Neem dan alvast een kijkje op <nuxt-link to="/spulmer-worden">deze pagina ...</nuxt-link> !
         </p>
       </v-col>
     </v-row>
