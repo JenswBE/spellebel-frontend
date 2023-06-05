0798d093c4019801bb8b1e51c90c494e2949c0ff
MarCanIT <info@MarCanIT.be>
2023-06-05 15:09:26 +0200

    afbeeldingen vakantie

--- pages/index.vue
+++ pages/index.vue
@@ -75,7 +75,7 @@
                 onze website en sociale media op de hoogte !
               </p>
               <p class="text-body-1">
-                <br /><v-img src="/img/nieuws/2022-12_Sint.jpeg" max-height="100">
+                <br /><v-img src="/img/nieuws/2022-12_Sint.jpg" max-height="100px">
                 <br />Ook in onze speel-o-theek waren er weer veel brave kindjes (en ouders) !
                 <br />De Sint liet weer heel wat nieuw speelgoed achter ...
               </p>
